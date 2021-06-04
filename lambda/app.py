f2e435b187c09e07162bb67cdf2ca229e8886549
Marek Kuczynski <marek.kuczynski@gmail.com>
2021-06-04 22:33:16 +0200

    Added Lambda Powertools, added downloading and uploading of code artifacts to S3.

--- lambda/app.py
+++ lambda/app.py
@@ -1,9 +1,24 @@
-import shutil, subprocess
+import boto3, shutil, subprocess, os
+from aws_lambda_powertools import Logger, Tracer
+
+# start logger and tracing of function
+logger = Logger()
+modules_to_be_patched = [ "boto3" ]
+tracer = Tracer(patch_modules = modules_to_be_patched)
+
+# connect to S3 bucket using AWS SDK
+s3_bucket_conn = boto3.client("s3")
+s3_bucket_name = os.environ['s3_bucket_name']
+
 
 # run subprocess
+@tracer.capture_method(capture_response = True)
 def subproc(cmd):
+
+    # run bash command in subprocess
     process = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True, cwd = '/tmp', text = True)
 
+    # get stdout, stderr and exit code
     stdout, stderr = process.communicate()
     exit_code = process.wait()
 
@@ -13,18 +28,44 @@ def subproc(cmd):
     print('exit_code ' + str(exit_code))
     print('')
 
+    return stdout
 
+
+# download file from S3
+@tracer.capture_method(capture_response = True)
+def s3_download_file(src, dst):
+
+    print('retrieving s3://' + s3_bucket_name + '/' + src + ' to ' + dst)
+    s3_bucket_conn.download_file(s3_bucket_name, src, dst)
+
+
+# upload file to S3
+@tracer.capture_method(capture_response = True)
+def s3_upload_file(src, dst):
+
+    print('uploading ' + src + ' to s3://'+ s3_bucket_name + '/' + dst)
+    s3_bucket_conn.upload_file(src, s3_bucket_name, dst)
+
+
+# lambda handler
+@logger.inject_lambda_context(log_event = True)
+@tracer.capture_lambda_handler(capture_response = True)
 def lambda_handler(event, context):
 
-    print('event ' + str(event))
-    print('')
-    cmd = event['command']
+    # get input parameters from event
+    cmd         = event['command']
+    inputf      = event['input_file']
+    outputf     = event['output_file']
+    tmpfile     = '/tmp/main.go'
 
-    shutil.copy('./main.go', '/tmp/main.go')
+    # download Go file from S3 to /tmp/main.go
+    s3_download_file(inputf, tmpfile)
 
-    subproc('ls -alFh /tmp')
-    subproc(cmd)
-    
-    subproc('ls -alFh /tmp')
+    # run build command
+    stdout = subproc(cmd)
 
-    return 'end'
+    # upload Go artifact to S3 from /tmp/outputcode
+    s3_upload_file('/tmp/outputcode', outputf)
+
+    # return build output
+    return stdout
--- readme.md
+++ readme.md
@@ -3,6 +3,17 @@ go-lambda-pipeline
 
 Build and run your Golang code in S3 using AWS Lambda. This allows you to create a serverless build service that outputs the artifacts to a bucket. 
 
-Deploy the stack using AWS SAM and invoke the Lambda function with the 'event.json' input to build the "hello world" sample. You can customize the event to include any build parameters (such as 'GOOS', 'GOARCH', etc.) or where the input and output files are located.
+Deploy the stack using AWS SAM and invoke the Lambda function with the 'event.json' input. 
+
+```
+{
+    "command": "cd /tmp && go build -o outputcode main.go",
+    "input_file": "main.go",
+    "output_file": "outputcode"
+}
+```
+
+You should specify the input and output paths for the code based on their S3 prefix paths from the included S3 bucket. You can customize the event to include any build parameters (such as 'GOOS', 'GOARCH', etc.).
+
 
 This is a very initial proof of concept right now, please use it for educational purposes only. 
